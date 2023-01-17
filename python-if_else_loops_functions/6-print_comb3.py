ff0dc341a3f442152d71bee2d061c82f50e4e9d0
agustinmeriles <agustinmeriles@hotmail.com>
2023-01-17 14:11:00 -0300

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for k in range(i + 1, 10):
-        print("{:d}{:d}".format(i, k), end=", ")
-print("89")
+        if i == 8:
+            print("{:d}{:d}".format(i, k))
+        else:
+            print("{:d}{:d}".format(i, k), end=", ")
