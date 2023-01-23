7e886793d4e603ef33d81905749958810346263d
Agus <agustinmeriles@hotmail.com>
2023-01-23 19:02:47 -0300

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        cont = 0
         for i in row:
-            print("{:>3}".format(i), end=" ")
+            cont += 1
+            if len(row) != cont:
+                print("{:d}".format(i), end=" ")
+            else:
+                print("{:d}".format(i), end="")
         print()
