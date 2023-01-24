37cf97627c3bc1d54d50451a31d6d5d31c166bf1
Agus <agustinmeriles@hotmail.com>
2023-01-24 10:45:17 -0300

    4-only_diff_elements.py

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    set_3 = set()
+    for i in set_1:
+        if i in set_2:
+            continue
+        else:
+            set_3.add(i)
+    for i in set_2:
+        if i in set_1:
+            continue
+        else:
+            set_3.add(i)
+    return set_3
