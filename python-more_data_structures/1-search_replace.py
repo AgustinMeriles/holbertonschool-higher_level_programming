98691997e3ff5add7101be771c5f4d5b7927c1e2
Agus <agustinmeriles@hotmail.com>
2023-01-24 09:47:41 -0300

    1-search_replace.py

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/urs/bin/python3
+def search_replace(my_list, search, replace):
+    new = my_list.copy()
+    for idx, item in enumerate(my_list):
+        if item == search:
+            new[idx] = replace
+    return new
