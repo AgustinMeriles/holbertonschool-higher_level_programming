915cec9afa4dad082fed7e7da00d9d8c5237c555
Agus <agustinmeriles@hotmail.com>
2023-01-25 15:33:36 -0300

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        while i < x:
+            print(my_list[i], end="")
+            i += 1
+        print()
+        return i
+    except:
+        while True:
+            try:
+                print(my_list[i], end="")
+                i += 1
+            except:
+                print()
+                return i
