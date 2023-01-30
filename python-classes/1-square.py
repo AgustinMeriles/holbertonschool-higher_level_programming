68553923c861084fcf0e46ec9518a62c99311d63
Agus <agustinmeriles@hotmail.com>
2023-01-30 02:05:46 -0300

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """
+    The Square class defines a square object.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square clas with
+        a given size
+
+        Args:
+        size (int): The size of the square.
+        """
+        self.__size = size
