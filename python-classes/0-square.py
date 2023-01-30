8a221ce8c18e870b47302688ac4c7e50c103a5f6
Agus <agustinmeriles@hotmail.com>
2023-01-30 02:00:48 -0300

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    The Square class defines a square object
+    """
+    def __init__(self):
+        """
+        Initializes a new instance of Square class
+        """
+        pass
