13bdab18700e3386833e14c9118d7ab9a5dd9f1e
Agus <agustinmeriles@hotmail.com>
2023-02-01 22:30:41 -0300

    2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,7 @@
 
 
 def matrix_divided(matrix, div):
+    """Inizializes"""
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
@@ -13,8 +14,8 @@ def matrix_divided(matrix, div):
         if len(row) != row_len:
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
+            msj = "matrix must be a matrix (list of lists) of integers/floats"
             if type(element) not in [int, float]:
-                raise TypeError("matrix must be a matrix \
-                    (list of lists) of integers/floats")
+                raise TypeError(msj)
 
     return [[round(element / div, 2) for element in row] for row in matrix]
