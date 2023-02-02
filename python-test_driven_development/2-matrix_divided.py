cb72dfd13aa7db7397348d0215891ed5767bac5d
Agus <agustinmeriles@hotmail.com>
2023-02-01 22:20:58 -0300

    2-matrix_divided.py

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,16 @@
+def matrix_divided(matrix, div):
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    row_len = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_len:
+            raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if type(element) not in [int, float]:
+                raise TypeError("matrix must be a matrix \
+                    (list of lists) of integers/floats")
+
+    return [[round(element / div, 2) for element in row] for row in matrix]
