0a46e3f7f0b3e1a8fde8791576d3412e3038aedb
Agus <agustinmeriles@hotmail.com>
2023-02-04 12:55:07 -0300

    fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """empty class Rectangle that defines a rectangle"""
     def __init__(self, width=0, height=0):
         self.width = width
-        self.heigth = height
+        self.height = height
 
     @property
     def width(self):
