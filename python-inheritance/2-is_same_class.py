b5965899203da5cfaa4c044040b5c05c4239d3a8
agustinmeriles <agustinmeriles@hotmail.com>
2023-02-07 11:15:58 -0300

    2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+""" returns True if the object is exactly an instance of the
+ specified class; otherwise False"""
+
+
+def is_same_class(obj, a_class):
+    """Inicialized"""
+    if type(obj) is a_class:
+        return True
+    return False
