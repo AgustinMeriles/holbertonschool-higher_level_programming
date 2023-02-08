76c6581923bd7d30d9b8892d5831769bf2daa990
Agus <agustinmeriles@hotmail.com>
2023-02-08 17:14:37 -0300

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Shebang"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define class Square"""
+
+    def __init__(self, size):
+        """Initialization"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
+
+    def area(self):
+        return self.__size * self.__size
