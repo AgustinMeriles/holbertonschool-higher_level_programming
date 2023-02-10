07219dc3858b1e2bfc258be2214caade96ce4e12
Agus <agustinmeriles@hotmail.com>
2023-02-10 13:06:51 -0300

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Shebang"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file"""
+
+    with open(filename, mode="r", encoding="utf-8") as f:
+        cont = 0
+        for x in f:
+            for i in x:
+                cont = cont + 1
+        return cont
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
