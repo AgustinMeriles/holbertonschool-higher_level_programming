a75eec6f6fb833b63a32b48ba4e1ee048b386d4d
Agus <agustinmeriles@hotmail.com>
2023-02-10 13:08:19 -0300

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,11 +5,11 @@
 def write_file(filename="", text=""):
     """function that writes a string to a text file"""
 
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
     with open(filename, mode="r", encoding="utf-8") as f:
         cont = 0
         for x in f:
             for i in x:
                 cont = cont + 1
         return cont
-    with open(filename, mode="w", encoding="utf-8") as f:
-        f.write(text)
