e01c150c2f814d50968f31060771d6b756fb3d68
agustinmeriles <agustinmeriles@hotmail.com>
2023-02-15 13:52:09 -0300

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Function that returns the json string representation"""
-        st = []
-        if not list_dictionaries:
-            return st
-        return json.dumps(list_dictionaries)
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
