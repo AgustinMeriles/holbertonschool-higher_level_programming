970fd76ab5d04ca883de9bd93390e9c8a761fa7e
agustinmeriles <agustinmeriles@hotmail.com>
2023-03-17 12:42:14 -0300

    15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT score, number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) FROM second_table GROUP BY score ORDER BY number DESC;
