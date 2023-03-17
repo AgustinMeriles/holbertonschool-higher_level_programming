7292935ad85b6c2d7e2b8919cd398b8884d00913
agustinmeriles <agustinmeriles@hotmail.com>
2023-03-17 12:49:43 -0300

    16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT score, COUNT(score) AS name FROM second_table GROUP BY score ORDER BY name DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
