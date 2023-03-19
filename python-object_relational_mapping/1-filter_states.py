f8b80b89bc57e66c9a7cc294deaa5c1d2c43b1a8
Agus <agustinmeriles@hotmail.com>
2023-03-19 20:46:59 -0300

    1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Shebang"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    mS_username = sys.argv[1]
+    mS_password = sys.argv[2]
+    d_name = sys.argv[3]
+
+    my_connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mS_username,
+        passwd=mS_password,
+        db=d_name)
+
+    cur = my_connection.cursor()
+    cur.excecute("SELECT * FROM states WHERE name REGEXP '^N'")
+
+    for state in cur.fetchall():
+        print(state)
