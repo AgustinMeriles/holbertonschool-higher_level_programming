9c4db438b201acf287bc73d9e2c1672d6d70ac46
Agus <agustinmeriles@hotmail.com>
2023-03-19 21:10:27 -0300

    Hot fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         db=d_name)
 
     cur = my_connection.cursor()
-    q = "SELECT * FROM states WHERE name REGEXP '^N' ORDER BY states.id ASC"
+    q = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
     cur.execute(q)
 
     for state in cur.fetchall():
