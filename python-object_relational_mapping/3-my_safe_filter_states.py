9db09c8e765e38920192ae96795a3e1956b32b3a
Agus <agustinmeriles@hotmail.com>
2023-03-19 21:39:02 -0300

    3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Shebang"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    mS_username = sys.argv[1]
+    mS_password = sys.argv[2]
+    d_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    my_connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mS_username,
+        passwd=mS_password,
+        db=d_name)
+
+    cur = my_connection.cursor()
+    q = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+    my_connection.excecute(q, (sys.argv[4],))
+
+    for element in cur.fetchall():
+        print(element)
