49a825f02bd494b3d9a62916df8fa806e04b75ba
Agus <agustinmeriles@hotmail.com>
2023-04-15 12:55:09 -0300

    8-square.js

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const number = parseInt(process.argv[2]);
+const txt = 'x';
+let i;
+let p = '';
+
+if (isNaN(number)) {
+  console.log('Missing size');
+} else {
+  for(i = 0; i < number; i++){
+    p = '';
+    for(let y = 0; y < number; y++){
+      p += txt;
+    }
+    console.log(p);
+  }
+}
