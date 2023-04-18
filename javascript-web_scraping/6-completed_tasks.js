d63518d710793d19037ae7d9c9f91de441dfeb02
Agus <agustinmeriles@hotmail.com>
2023-04-18 20:12:20 -0300

    fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,17 +3,17 @@
 const request = require('request');
 const apiUrl = process.argv[2];
 
-request.get({ apiUrl }, (error, response, data) => {
+request.get({ url: apiUrl }, (error, response, data) => {
   if (error) {
     console.error(error);
   } else {
-    const tasks = data;
+    const tasks = JSON.parse(data);
     const completedTskByUser = {};
 
     tasks.forEach(task => {
       if (task.completed) {
-        const userId = task.id;
-        completedTskByUser[userId]++;
+        const userId = task.userId;
+        completedTskByUser[userId] = (completedTskByUser[userId] || 0) + 1;
       }
     });
     console.log(completedTskByUser);
