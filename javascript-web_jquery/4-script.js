bd24d91b1f05a5afcd38840b810d2b835b4f9d53
Agus <agustinmeriles@hotmail.com>
2023-04-19 17:32:12 -0300

    4-script.js

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("div#toggle_header").click(function() {
+      var currentClass = $("header").attr("class");
+      if (currentClass == "green") {
+        $("header").removeClass("green").addClass("red");
+      } else {
+        $("header").removeClass("red").addClass("green");
+      }
+    });
+  });
